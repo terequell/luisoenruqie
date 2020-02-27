0ff6127c4799af357076cdda29729231061c669c
Alexey <alexey-032@ya.ru>
2020-02-27 14:48:28 +0300

    upgraded slider on testimonials section

--- js/script.js
+++ js/script.js
@@ -1,20 +1,46 @@
 var carouselButtons = document.getElementsByClassName('carousel__pics')
 var limaraItems = document.getElementsByClassName('tst__limara')
 for (var i = 0; i<carouselButtons.length; i++) {
-   carouselButtons[i].addEventListener('click', switchTestimonial)
+   carouselButtons[i].addEventListener('click', (event) => {
+      switchTestimonialClicked(event), 
+      currentIndex = getNumberCurrentButton()
+   })
    if (i>0) {
       limaraItems[i].style.display = 'none'
    }
 }
 
-function switchTestimonial (event) {
+setInterval(switchTesimonialAuto, 2000)
+var currentIndex = 0
+
+function switchTestimonialClicked (event) {
    var button = event.target
+   switchCurrentButton(button)
+   switchCurrentItem()
+}
+
+function switchTesimonialAuto () {
+   var button = carouselButtons[currentIndex]
+   switchCurrentButton(button)
+   switchCurrentItem()
+   if (currentIndex >= carouselButtons.length-1) {
+      currentIndex = 0
+   }  else {
+      currentIndex = currentIndex + 1 
+   }
+}
+
+// меняет текущий "квадратик" снизу блока, добавляя ему класс current
+function switchCurrentButton (button) {
    var currentButton = document.getElementsByClassName('current')
    currentButton[0].classList.remove('current')
    button.classList.add('current')
-   var currentButtonNum = getNumberCurrentButton()
+}
+
+// меняет текущий блок в зависимости от текущего "квадратика", т.е синхронизирует их
+function switchCurrentItem () {
    for (var i = 0; i< limaraItems.length; i++) {
-      if (i === currentButtonNum) {
+      if (i === getNumberCurrentButton()) {
          limaraItems[i].style.display = 'flex'
       }
       else {
