3c077009ef4cd8d72d3110e35c6f620b7eda604b
Alexey <alexey-032@ya.ru>
2020-03-08 14:53:58 +0300

    many many fixes and new things

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 var carouselButtons = document.getElementsByClassName('carousel__pics')
 var limaraItems = document.getElementsByClassName('tst__limara')
-for (var i = 0; i<carouselButtons.length; i++) {
+for (var i = 0; i < carouselButtons.length; i++) {
    carouselButtons[i].addEventListener('click', (event) => {
-      switchTestimonialClicked(event), 
-      currentIndex = getNumberCurrentButton()
+      switchTestimonialClicked(event),
+         currentIndex = getNumberCurrentButton()
    })
-   if (i>0) {
+   if (i > 0) {
       limaraItems[i].style.display = 'none'
    }
 }
@@ -13,37 +13,36 @@ for (var i = 0; i<carouselButtons.length; i++) {
 setInterval(switchTesimonialAuto, 2000)
 var currentIndex = 0
 
-function switchTestimonialClicked (event) {
+function switchTestimonialClicked(event) {
    var button = event.target
    switchCurrentButton(button)
    switchCurrentItem()
 }
 
-function switchTesimonialAuto () {
+function switchTesimonialAuto() {
    var button = carouselButtons[currentIndex]
    switchCurrentButton(button)
    switchCurrentItem()
-   if (currentIndex >= carouselButtons.length-1) {
+   if (currentIndex >= carouselButtons.length - 1) {
       currentIndex = 0
-   }  else {
-      currentIndex = currentIndex + 1 
+   } else {
+      currentIndex = currentIndex + 1
    }
 }
 
 // меняет текущий "квадратик" снизу блока, добавляя ему класс current
-function switchCurrentButton (button) {
+function switchCurrentButton(button) {
    var currentButton = document.getElementsByClassName('current')
    currentButton[0].classList.remove('current')
    button.classList.add('current')
 }
 
 // меняет текущий блок в зависимости от текущего "квадратика", т.е синхронизирует их
-function switchCurrentItem () {
-   for (var i = 0; i< limaraItems.length; i++) {
+function switchCurrentItem() {
+   for (var i = 0; i < limaraItems.length; i++) {
       if (i === getNumberCurrentButton()) {
          limaraItems[i].style.display = 'flex'
-      }
-      else {
+      } else {
          limaraItems[i].style.display = 'none'
       }
    }
@@ -55,4 +54,26 @@ function getNumberCurrentButton() {
          return i
       }
    }
+}
+
+// 
+
+var formFooter = document.getElementById('footer_form')
+
+// Adaptive menu
+
+var menuButton = document.getElementById('heading__nav-menuBurger')
+menuButton.addEventListener('click', () => {
+   var menuElems = document.getElementsByClassName('heading__nav-items')[0]
+   menuElems.classList.toggle('activeMenu')
+   menuButton.classList.toggle('activeBurger')
+})
+
+
+var menuElems = document.getElementsByClassName('heading__nav-items')[0]
+for (let i = 0; i < menuElems.children.length; i++) {
+   menuElems.children[i].addEventListener('click', () => {
+      menuElems.classList.remove('activeMenu')
+      menuButton.classList.remove('activeBurger')
+   })
 }
